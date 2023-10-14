4018309cc7c30f1bbe677698d82d5645ad228a39
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-14 14:41:00 -0300

    Agrego repositories

---
+++ src/main/java/luceroraul12/challenge/automation/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package luceroraul12.challenge.automation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import luceroraul12.challenge.automation.entity.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+
+}
---
+++ src/main/java/luceroraul12/challenge/automation/repository/TipoProductoRepository.java
@@ -0,0 +1,9 @@
+package luceroraul12.challenge.automation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import luceroraul12.challenge.automation.entity.TipoProducto;
+
+public interface TipoProductoRepository extends JpaRepository<TipoProducto, Integer>{
+
+}
