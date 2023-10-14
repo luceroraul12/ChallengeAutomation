328f78eb179f968a5c74202010a91ed11e1afcab
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-14 14:42:39 -0300

    Conecto controller con el service

--- src/main/java/luceroraul12/challenge/automation/controller/AutomationController.java
+++ src/main/java/luceroraul12/challenge/automation/controller/AutomationController.java
@@ -2,6 +2,7 @@ package luceroraul12.challenge.automation.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,39 +14,38 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.websocket.server.PathParam;
 import luceroraul12.challenge.automation.dto.ProductoDto;
 import luceroraul12.challenge.automation.dto.ProductoStock;
+import luceroraul12.challenge.automation.service.ProductoService;
 
 @RestController
 @RequestMapping("/automation")
 public class AutomationController {
 	
+	@Autowired
+	ProductoService service;
+	
 	@GetMapping("/producto")
 	public List<ProductoDto> obtenerProductos() {
-		
-		return null;
+		return service.obtenerProductos();
 	}
 	
 	@PostMapping("/producto")
 	public ProductoDto crearProducto(@RequestBody ProductoDto dto) {
-		
-		return null;
+		return service.crearProducto(dto);
 	}
 	
 	@PutMapping("/producto")
 	public ProductoDto actualizarProducto(@RequestBody ProductoDto dto) {
-		
-		return null;
+		return service.actualizarProducto(dto);
 	}
 
 	@DeleteMapping("/producto/{idProducto}")
 	public Integer eliminarProducto(@PathParam(value = "idProducto") Integer idProducto) {
-		
-		return null;
+		return service.eliminarProducto(idProducto);
 	}
 	
 	@GetMapping("/stock")
 	public List<ProductoStock> obtenerStockProductos() {
-		
-		return null;
+		return service.obtenerStockProductos();
 	}
 	
 }
