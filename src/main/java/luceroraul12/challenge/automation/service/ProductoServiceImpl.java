e7ea630c176cb76d4874fc9fb5f2788c4f558c18
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2023-10-16 10:04:56 -0300

    Idento y agrego ordenamiento de producto y stock  por nombre de producto

--- src/main/java/luceroraul12/challenge/automation/service/ProductoServiceImpl.java
+++ src/main/java/luceroraul12/challenge/automation/service/ProductoServiceImpl.java
@@ -1,5 +1,6 @@
 package luceroraul12.challenge.automation.service;
 
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,31 +19,32 @@ import luceroraul12.challenge.automation.repository.StockProductoRepository;
 import luceroraul12.challenge.automation.repository.TipoProductoRepository;
 
 @Service
-public class ProductoServiceImpl implements ProductoService{
-	
+public class ProductoServiceImpl implements ProductoService {
+
 	@Autowired
 	private ProductoRepository productoRepository;
-	
+
 	@Autowired
 	private TipoProductoRepository tipoProductoRepository;
-	
+
 	@Autowired
 	private ProductoConverter productoConverter;
-	
+
 	@Autowired
 	private TipoProductoConverter tipoProductoConverter;
-	
+
 	@Autowired
 	private StockProductoRepository stockProductoRepository;
-	
+
 	@Autowired
 	private ProductoStockConverter productoStockConverter;
-	
-	
 
 	@Override
 	public List<ProductoDto> obtenerProductos() {
-		return productoConverter.toContratoList(productoRepository.findAll());
+		// Ordeno los productos por nombre de productos
+		List<Producto> productos = productoRepository.findAll();
+		productos.sort((a, b) -> a.getNombre().compareTo(b.getNombre()));
+		return productoConverter.toContratoList(productos);
 	}
 
 	@Override
@@ -53,7 +55,7 @@ public class ProductoServiceImpl implements ProductoService{
 		Producto producto = productoConverter.toEntidad(dto);
 		// Persisto
 		producto = productoRepository.save(producto);
-		
+
 		return productoConverter.toContrato(producto);
 	}
 
@@ -62,25 +64,29 @@ public class ProductoServiceImpl implements ProductoService{
 		// Valido
 		validarProductoDto(dto);
 		// Valido que contenga identificador
-		if(dto.getId() == null)
-			throw new Exception("Nos debe pasar el identificador del producto a modificar");
+		if (dto.getId() == null)
+			throw new Exception(
+					"Nos debe pasar el identificador del producto a modificar");
 		// Valido que exista en la base de datos
-		Producto oldProducto = productoRepository.findById(dto.getId()).orElse(null);
-		if(oldProducto == null)
-			throw new Exception("El producto que se quiere modificar no existe en la base de datos");
+		Producto oldProducto = productoRepository.findById(dto.getId())
+				.orElse(null);
+		if (oldProducto == null)
+			throw new Exception(
+					"El producto que se quiere modificar no existe en la base de datos");
 		// Convierto
 		Producto producto = productoConverter.toEntidad(dto);
 		// Persisto
 		producto = productoRepository.save(producto);
-		
+
 		return productoConverter.toContrato(producto);
 	}
 
 	@Override
 	public Integer eliminarProducto(Integer idProducto) throws Exception {
 		// Valido identificador
-		if(idProducto == null)
-			throw new Exception("Nos debe pasar el identificador del producto a modificar");
+		if (idProducto == null)
+			throw new Exception(
+					"Nos debe pasar el identificador del producto a modificar");
 		productoRepository.deleteById(idProducto);
 		return idProducto;
 	}
@@ -88,23 +94,32 @@ public class ProductoServiceImpl implements ProductoService{
 	@Override
 	public List<ProductoStockDto> obtenerStockProductos() {
 		List<StockProducto> stocks = stockProductoRepository.findAll();
+		// Ordeno por nombre de producto
+		stocks.sort((a, b) -> a.getProducto().getNombre()
+				.compareTo(b.getProducto().getNombre()));
 		return productoStockConverter.toContratoList(stocks);
 	}
-	
+
 	@Override
-	public ProductoStockDto actualizarStockProducto(ProductoStockDto dto) throws Exception{
+	public ProductoStockDto actualizarStockProducto(ProductoStockDto dto)
+			throws Exception {
 		// Valido dto
-		if(dto == null)
-			throw new Exception("Nos debe pasar el stock que se quiere actualizar.");
-		if(dto.getId() == null)
-			throw new Exception("Nos debe pasar el identificador del stock a actualizar");
-		if(dto.getCantidad() == null)
-			throw new Exception("Nos debe pasar la cantidad de producto a la que se quiere actualizar");
+		if (dto == null)
+			throw new Exception(
+					"Nos debe pasar el stock que se quiere actualizar.");
+		if (dto.getId() == null)
+			throw new Exception(
+					"Nos debe pasar el identificador del stock a actualizar");
+		if (dto.getCantidad() == null)
+			throw new Exception(
+					"Nos debe pasar la cantidad de producto a la que se quiere actualizar");
 		// Valido que exista
-		StockProducto oldStock = stockProductoRepository.findById(dto.getId()).orElse(null);
-		if(oldStock == null)
-			throw new Exception("El stock que quiere actualizar no existe en la base de datos");
-		
+		StockProducto oldStock = stockProductoRepository.findById(dto.getId())
+				.orElse(null);
+		if (oldStock == null)
+			throw new Exception(
+					"El stock que quiere actualizar no existe en la base de datos");
+
 		// En la actualizacion solo voy a actualizar las cantidades
 		oldStock.setCantidad(dto.getCantidad());
 		stockProductoRepository.save(oldStock);
@@ -113,24 +128,23 @@ public class ProductoServiceImpl implements ProductoService{
 
 	@Override
 	public List<TipoProductoDto> obtenerTipoProductos() {
-		return tipoProductoConverter.toDtoList(tipoProductoRepository.findAll());
+		return tipoProductoConverter
+				.toDtoList(tipoProductoRepository.findAll());
 	}
 
-	
-	private void validarProductoDto(ProductoDto dto) throws Exception{
+	private void validarProductoDto(ProductoDto dto) throws Exception {
 		// Validaciones
-		if(dto == null)
+		if (dto == null)
 			throw new Exception("Nos debe pasar el producto a crear");
-		
-		if(dto.getNombre() == null)
+
+		if (dto.getNombre() == null)
 			throw new Exception("Nos debe pasar el nombre del producto");
-		
-		if(dto.getPrecio() == null)
+
+		if (dto.getPrecio() == null)
 			throw new Exception("Nos debe pasar el precio del producto");
-		
-		if(dto.getTipo() == null)
+
+		if (dto.getTipo() == null)
 			throw new Exception("Nos debe pasar el tipo de producto");
 	}
 
-	
 }
